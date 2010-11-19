9810008a20edc26e3cd616296fd01ac463b39ae5
kruecker <>
2010-11-19 15:39:56 +0000

    duplicate check in AllRootFilesNoDup

--- examples/NtupleTools2.h
+++ examples/NtupleTools2.h
@@ -1,6 +1,7 @@
 //---NtupleTools----------
 //   Version 2.0
 //   15.07.2010
+//   19.11.2010 duplicate check in AllRootFilesNoDup
 //   dirk.kruecker@desy.de
 
 #ifndef NtupleTools2_h
@@ -15,6 +16,7 @@
 #include <map>
 #include <vector>
 #include <cstdlib>
+#include <algorithm>
 #include "TTree.h"
 #include "TChain.h"
 #include "TSystem.h"
@@ -32,7 +34,7 @@ typedef ROOT::Math::DisplacementVector3D<ROOT::Math::Cartesian3D<double> > XYZVe
 typedef ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<double>  >     XYZPointD;
 
 #ifdef __MAKECINT__
-#pragma link C++ class pair<string,bool>+;
+//#pragma link C++ class pair<string,bool>+;
 #pragma link C++ class vector<LorentzV>+;
 #pragma link C++ class map<string,bool>+;
 #endif
@@ -67,7 +69,7 @@ void progressT(int flush=0,ostream& os=cout){
 };
 
 //---------- file handling -----------------------------
-int GetResult(vector<string>& out, const string& command){
+int GetResult(vector<string>& out, const string& command,bool nodup){
 	TString line;
 	FILE* pipe= gSystem->OpenPipe(command.c_str(),"r");
 	if(!pipe){ 
@@ -79,30 +81,77 @@ int GetResult(vector<string>& out, const string& command){
 		}
 		gSystem->ClosePipe(pipe);
 	}
+	if(nodup){
+		map<string, pair<unsigned,string> > singleOut;
+		map<string, pair<unsigned,string> >::iterator it;
+		unsigned i;
+		for(i=0;i<out.size();i++){
+			//check format
+			if(count(out[i].begin(),out[i].end(),'_')!=4) break;
+			//let's hope it fits
+			unsigned pos =out[i].rfind("_");
+			unsigned pos2=out[i].substr(0,pos).rfind("_")+1;
+			unsigned n=atoi(out[i].substr(pos2,pos-pos2).c_str());
+			it = singleOut.find(out[i].substr(0,pos2));
+			if(it!=singleOut.end()) {
+				if(it->second.first<n) {
+					it->second.first=n;
+					it->second.second=out[i].substr(pos2);
+				}
+			} else singleOut[out[i].substr(0,pos2)]=pair<unsigned,string>(n,out[i].substr(pos2));
+		}
+		if(i==out.size()){
+			cout<< out.size()-singleOut.size() <<" duplicates ignored!"<<endl;;
+			out.clear();
+			for(it = singleOut.begin();it!=singleOut.end();it++)
+			 		out.push_back(it->first+it->second.second);
+		} else cout<<"File name format not appropriate for duplicate check!"<<endl;
+	}
 	return out.size();
 }
-int AllRootFilesIn(const string& dir,TChain* chain,const string& LScommand="ls",int max=1000){
+//AllRootFilesIn(dir,tree)           // any 'ls'-able directory
+//AllRootFilesIn(dir,tree,10)        // any 'ls'-able directory, first 10 files
+//AllRootFilesIn(dir,tree,"dcls")    // dcache -  needs proxy & dctools !
+//AllRootFilesIn(dir,tree,"dcls",10) //first 10 files in dir                              
+int AllRootFilesIn(const string& dir,TChain* chain,const string& LScommand,int max,bool nodup=false){
 	vector<string> files;
 	int n=0;
 	if(LScommand=="dcls"){
-		n=GetResult(files,"dcls "+dir+" | grep \"\\.root\" ");
+		n=GetResult(files,"dcls "+dir+" | grep \"\\.root\" ",nodup);
 		n=n>max?max:n; 
 		const string dcache_gate="dcap://dcache-ses-cms.desy.de:22125/";
 		for(int i=0;i<n;++i) chain->Add((dcache_gate+dir+"/"+files[i]).c_str());
 	} else if(LScommand=="rfdir") {
-		n=GetResult(files,LScommand+" "+dir+" | grep \"\\.root\" | awk \'{print $9}\' ");	
+		n=GetResult(files,LScommand+" "+dir+" | grep \"\\.root\" | awk \'{print $9}\' ",nodup);	
 		n=n>max?max:n; 
 		for(int i=0;i<n;++i) chain->Add(("rfio:"+dir+"/"+files[i]).c_str());
 	} else {
-		n=GetResult(files,LScommand+" "+dir+" | grep \"\\.root\"");	
+		n=GetResult(files,LScommand+" "+dir+" | grep \"\\.root\"",nodup);
 		n=n>max?max:n; 
 		for(int i=0;i<n;++i) chain->Add((dir+"/"+files[i]).c_str());
 	}
 	return n;
 };
-void AllRootFilesIn(const string& dir,TChain* chain,int max=1000,const string& LScommand="ls"){
-	AllRootFilesIn(dir,chain,LScommand,max);
-};
+// no duplicate check
+int AllRootFilesIn(const string& dir,TChain* chain){
+	return AllRootFilesIn(dir,chain,"ls",1000,false);
+}
+int AllRootFilesIn(const string& dir,TChain* chain,const string& LScommand){
+	return AllRootFilesIn(dir,chain,LScommand,1000,false);
+}
+int AllRootFilesIn(const string& dir,TChain* chain,int max){
+	return AllRootFilesIn(dir,chain,"ls",max,false);
+}
+// duplicate check
+int AllRootFilesNoDup(const string& dir,TChain* chain){
+	return AllRootFilesIn(dir,chain,"ls",1000,true);
+}
+int AllRootFilesNoDup(const string& dir,TChain* chain,const string& LScommand){
+	return AllRootFilesIn(dir,chain,LScommand,1000,true);
+}
+int AllRootFilesNoDup(const string& dir,TChain* chain,int max){
+	return AllRootFilesIn(dir,chain,"ls",max,true);
+}
 
 //---------- chain and tree handling -----------------------------
 // inline is redundant since this in a header files
