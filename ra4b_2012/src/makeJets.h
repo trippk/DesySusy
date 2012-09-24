84c2d2b4ebab1945ae836392842389223942b78d
msahin <>
2012-09-24 22:11:27 +0000

    synch

--- ra4b_2012/src/makeJets.h
+++ ra4b_2012/src/makeJets.h
@@ -6,5 +6,5 @@
 #include "Muon.h"
 #include "Electron.h"
 vector<Jet> makeJets(EasyChain* tree );
-void makeCleanedJets(vector<Jet*>& Jets_In, vector<Jet*>& Jets_Out, vector<Muon*>& Muons, vector<Electron*>& Electrons);
+void makeCleanedJets(vector<Jet*>& Jets_In, vector<Jet*>& Jets_Out, vector<Muon> Muons, vector<Electron> Electrons);
 #endif
