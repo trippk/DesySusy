0026249e8945ada1fde731258b4af101648eff1d
dhorton <>
2012-10-19 14:25:07 +0000

    Added diLep cut set.

--- ra4b_2012/src/SetConditions_RA4b.cpp
+++ ra4b_2012/src/SetConditions_RA4b.cpp
@@ -162,6 +162,35 @@ void SetConditions_RA4b(SampleInformation mySampleInformation){
     //============================================
     
   }
+  else if(mySampleInformation.GetEstimation()=="RA4b-diMu" ){
+    
+    //============================================
+    //ELECTRONS
+    //============================================
+    SetOfCuts::SignalElectrons.NUM.Set(-1,"above","any signal electron");
+    SetOfCuts::WideElectrons.NUM.Set(-1,"above","any wider electrons");
+    //============================================
+    // MUONS
+    //============================================
+    SetOfCuts::SignalMuons.NUM.Set(1,"above","more than one muon");
+    SetOfCuts::WideMuons.NUM.Set(-1,"equal","any wider muons");
+    //============================================
+    //LEPTONS
+    //============================================
+    SetOfCuts::Leptons.NUM.Set(1,"above","more than one lepton");
+    //============================================
+    //JETS
+    //============================================
+    SetOfCuts::Jets.NUM.Set(1,"above","jets>2");
+    //============================================
+    //EVENT
+    //============================================
+    SetOfCuts::Event.NumberOfBtags.Set(-1,"above","any number of btags");
+    SetOfCuts::Event.HT.Set(100.0,"above","HT above 100");
+    SetOfCuts::Event.MET.Set(0.,"above","MET above 0");
+    //============================================
+    
+  }
   else{
     cout<<"The estimation "<<mySampleInformation.GetEstimation()<<" does not exist"<<endl;
     exit(1);
