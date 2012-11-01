3a0860a81b09f853a3320519f2a57e7583c8a634
dhorton <>
2012-11-01 15:02:33 +0000

    Minor change

--- ra4b_2012/src/libRA4bDict_Linkdef.h
+++ ra4b_2012/src/libRA4bDict_Linkdef.h
@@ -3,5 +3,6 @@
 #pragma link off all classes;
 #pragma link off all functions;
 #pragma link C++ nestedclasses;
+//#pragme link C++ class vector<int>+;
 #pragma link C++ defined_in "anDiLep.h";
 #endif
