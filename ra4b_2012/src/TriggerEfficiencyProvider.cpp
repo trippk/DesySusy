e2b3eb6e835642a802125b53434ef0114d0a2598
fcostanz <>
2013-05-16 10:10:49 +0000

    TriggerEfficiencies

---
+++ ra4b_2012/src/TriggerEfficiencyProvider.cpp
@@ -0,0 +1,109 @@
+#include "TriggerEccifiencyProvider.h"
+
+//-------------------------------------------
+// efficiency for single lepton trigger
+//-------------------------------------------
+
+float gettrigweight(int id1, float pt, float eta){
+
+  //electron efficiencies
+  if ( abs(id1)==11 ) {
+    if ( fabs(eta)<1.5) {
+      if ( pt>=20 && pt<22 ) return 0.00;
+      if ( pt>=22 && pt<24 ) return 0.00;
+      if ( pt>=24 && pt<26 ) return 0.00;
+      if ( pt>=26 && pt<28 ) return 0.08;
+      if ( pt>=28 && pt<30 ) return 0.61;
+      if ( pt>=30 && pt<32 ) return 0.86;
+      if ( pt>=32 && pt<34 ) return 0.88;
+      if ( pt>=34 && pt<36 ) return 0.90;
+      if ( pt>=36 && pt<38 ) return 0.91;
+      if ( pt>=38 && pt<40 ) return 0.92;
+      if ( pt>=40 && pt<50 ) return 0.94;
+      if ( pt>=50 && pt<60 ) return 0.95;
+      if ( pt>=60 && pt<80 ) return 0.96;
+      if ( pt>=80 && pt<100 ) return 0.96;
+      if ( pt>=100 && pt<150 ) return 0.96;
+      if ( pt>=150 && pt<200 ) return 0.97;
+      if ( pt>=200 ) return 0.97;
+    } else if ( fabs(eta)>=1.5 && fabs(eta)<2.1) {
+      if ( pt>=20 && pt<22 ) return 0.00;
+      if ( pt>=22 && pt<24 ) return 0.00;
+      if ( pt>=24 && pt<26 ) return 0.02;
+      if ( pt>=26 && pt<28 ) return 0.18;
+      if ( pt>=28 && pt<30 ) return 0.50;
+      if ( pt>=30 && pt<32 ) return 0.63;
+      if ( pt>=32 && pt<34 ) return 0.68;
+      if ( pt>=34 && pt<36 ) return 0.70;
+      if ( pt>=36 && pt<38 ) return 0.72;
+      if ( pt>=38 && pt<40 ) return 0.74;
+      if ( pt>=40 && pt<50 ) return 0.76;
+      if ( pt>=50 && pt<60 ) return 0.77;
+      if ( pt>=60 && pt<80 ) return 0.78;
+      if ( pt>=80 && pt<100 ) return 0.80;
+      if ( pt>=100 && pt<150 ) return 0.79;
+      if ( pt>=150 && pt<200 ) return 0.76;
+      if ( pt>=200 ) return 0.81;
+    }
+  } else if ( abs(id1)==13 ) {//muon efficiencies
+
+    if ( fabs(eta)<0.8 ) {
+      if (pt>=20 && pt<22)  return  0.00;     
+      if (pt>=22 && pt<24)  return  0.03;      
+      if (pt>=24 && pt<26)  return  0.87;
+      if (pt>=26 && pt<28)  return  0.90;
+      if (pt>=28 && pt<30)  return  0.91;
+      if (pt>=30 && pt<32)  return  0.91;
+      if (pt>=32 && pt<34)  return  0.92;
+      if (pt>=34 && pt<36)  return  0.93;
+      if (pt>=36 && pt<38)  return  0.93;
+      if (pt>=38 && pt<40)  return  0.93;
+      if (pt>=40 && pt<50)  return  0.94;
+      if (pt>=50 && pt<60)  return  0.95;
+      if (pt>=60 && pt<80)  return  0.95;
+      if (pt>=80 && pt<100) return 0.94;
+      if (pt>=100 && pt<150) return 0.94;
+      if (pt>=150 && pt<200) return 0.93;
+      if (pt>=200) return 0.92;
+    } else if ( fabs(eta)>=0.8 && fabs(eta)<1.5 ) {
+      if (pt>=20 && pt<22)  return  0.00;
+      if (pt>=22 && pt<24)  return  0.05;
+      if (pt>=24 && pt<26)  return  0.78;
+      if (pt>=26 && pt<28)  return  0.81;
+      if (pt>=28 && pt<30)  return  0.81;
+      if (pt>=30 && pt<32)  return  0.81;
+      if (pt>=32 && pt<34)  return  0.82;
+      if (pt>=34 && pt<36)  return  0.82;
+      if (pt>=36 && pt<38)  return  0.83;
+      if (pt>=38 && pt<40)  return  0.83;
+      if (pt>=40 && pt<50)  return  0.84;
+      if (pt>=50 && pt<60)  return  0.84;
+      if (pt>=60 && pt<80)  return  0.84;
+      if (pt>=80 && pt<100) return 0.84;
+      if (pt>=100 && pt<150) return 0.84;
+      if (pt>=150 && pt<200) return 0.84;
+      if (pt>=200) return 0.82;
+    } else if ( fabs(eta)>=1.5 && fabs(eta)<2.1 ) {
+      if (pt>=20 && pt<22)  return  0.00;
+      if (pt>=22 && pt<24)  return  0.11;
+      if (pt>=24 && pt<26)  return  0.76;
+      if (pt>=26 && pt<28)  return  0.78;
+      if (pt>=28 && pt<30)  return  0.79;
+      if (pt>=30 && pt<32)  return  0.80;
+      if (pt>=32 && pt<34)  return  0.80;
+      if (pt>=34 && pt<36)  return  0.81;
+      if (pt>=36 && pt<38)  return  0.81;
+      if (pt>=38 && pt<40)  return  0.82;
+      if (pt>=40 && pt<50)  return  0.82;
+      if (pt>=50 && pt<60)  return  0.83;
+      if (pt>=60 && pt<80)  return  0.83;
+      if (pt>=80 && pt<100) return 0.83;
+      if (pt>=100 && pt<150) return 0.83;
+      if (pt>=150 && pt<200) return 0.82;
+      if (pt>=200) return 0.82;
+    }
+  }//end check for muons
+
+  return 1.;
+
+}
---
+++ ra4b_2012/src/TriggerEfficiencyProvider.h
@@ -0,0 +1,6 @@
+#ifndef TriggerEfficiencyProvider_h
+#define TriggerEfficiencyProvider_h
+
+float gettrigweight(int id1, float pt, float eta);
+
+#endif
